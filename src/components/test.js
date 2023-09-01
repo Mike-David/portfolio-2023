799542a4ee0d187b6de19b5ec4ca0bfde6d0121b
Michael Zbikowski <mike.zbik@gmail.com>
2023-09-02 00:49:56 +0200

    updated react router

---
+++ src/components/test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const InsertNameHere = () => {
+  return (
+    <div>
+      <h1>This is the InsertNameHere Page</h1>
+    </div>
+  );
+};
+
+export default InsertNameHere;
